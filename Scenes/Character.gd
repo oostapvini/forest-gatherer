09a18b5e3d245013c180b458512e5a3ab23407b5
oostapvini <oostap.vini@pm.me>
2026-04-07 13:18:50 +0300

    Implement player with movement and input
    Implemented player character with basic movement and input handling
    
    - created player scene and added it to Lvl0
    - configured Input Map for movement controls
    - implemented movement logic
    - added sprite flipping based on movement direction

---
+++ Scenes/Character.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -420.0
+
+@onready var anim : AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.wada
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("Left", "Right")
+	if direction:
+		velocity.x = direction * SPEED
+		anim.flip_h = direction > 0
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
